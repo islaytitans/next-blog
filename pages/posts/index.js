e54bfdb65ed5dd916c8e067064e2470bcd64f7c4
jonathan robbins <jonathan.m.robbins@gmail.com>
2021-10-31 11:29:06 +0000

    Added all posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,42 @@
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting started with Next.js",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next.js is a React framework for production - it makes building fullstack React apps a breeze and includes SSR.",
+    date: "2021-12-01",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting started with Next.js",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next.js is a React framework for production - it makes building fullstack React apps a breeze and includes SSR.",
+    date: "2022-01-01",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting started with Next.js",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next.js is a React framework for production - it makes building fullstack React apps a breeze and includes SSR.",
+    date: "2022-02-01",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting started with Next.js",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "Next.js is a React framework for production - it makes building fullstack React apps a breeze and includes SSR.",
+    date: "2022-03-01",
+  },
+];
+
 function AllPostsPage() {
-  return (<h1>All posts</h1>);
+  return (<AllPosts posts={DUMMY_POSTS} />);
 }
 
 export default AllPostsPage;
