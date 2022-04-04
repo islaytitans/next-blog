2ca7a6f38156d3aed375393d011bcd9d9d5dfa31
Jonathan Robbins <Jonathan.Robbins@rcn.org.uk>
2022-04-04 14:39:08 +0100

    Added getStaticPaths to define the static paths available once generated

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,35 @@
 import PostContent from "../../components/posts/post-detail/post-content";
+import { getPostData, getPostsFilenames } from "../../lib/posts-util";
 
-function PostDetailPage() {
-  return (<PostContent />);
+function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const post = getPostData(slug);
+
+  return {
+    props: {
+      post: post,
+    },
+    revalidate: 1800,
+  };
+}
+
+export function getStaticPaths() {
+  const allPostsFilenames = getPostsFilenames();
+
+  const slugs = allPostsFilenames.map((filename) =>
+    filename.replace(/\.md$/, "")
+  );
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
 }
 
 export default PostDetailPage;
